bd5d67b97e164da701fd099e80b1e3555ea9ef52
multicoloredwiz@gmail.com <multicoloredwiz@gmail.com@464adf66-71b8-e112-a980-cbfb9e40d06c>
2012-03-25 22:31:47 +0000

    Committed main.sql that contains the serial column on every relevant table.

--- athena/sql-files/main.sql
+++ athena/sql-files/main.sql
@@ -1,7 +1,4 @@
---
--- Table structure for table `auction`
---
-
+DROP TABLE IF EXISTS `auction`;
 CREATE TABLE IF NOT EXISTS `auction` (
   `auction_id` bigint(20) unsigned NOT NULL auto_increment,
   `seller_id` int(11) unsigned NOT NULL default '0',
@@ -21,13 +18,11 @@ CREATE TABLE IF NOT EXISTS `auction` (
   `card1` smallint(11) NOT NULL default '0',
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`auction_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `cart_inventory`
---
-
+DROP TABLE IF EXISTS `cart_inventory`;
 CREATE TABLE IF NOT EXISTS `cart_inventory` (
   `id` int(11) NOT NULL auto_increment,
   `char_id` int(11) NOT NULL default '0',
@@ -41,15 +36,14 @@ CREATE TABLE IF NOT EXISTS `cart_inventory` (
   `card1` smallint(11) NOT NULL default '0',
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
+  `price` tinyint(8) unsigned NOT NULL default '0',
   `expire_time` int(11) unsigned NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `char`
---
-
+DROP TABLE IF EXISTS `char`;
 CREATE TABLE IF NOT EXISTS `char` (
   `char_id` int(11) unsigned NOT NULL auto_increment,
   `account_id` int(11) unsigned NOT NULL default '0',
@@ -111,12 +105,9 @@ CREATE TABLE IF NOT EXISTS `char` (
   KEY `guild_id` (`guild_id`),
   KEY `name` (`name`),
   KEY `online` (`online`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=150000 ;
 
---
--- Table structure for table `charlog`
---
-
+DROP TABLE IF EXISTS `charlog`;
 CREATE TABLE IF NOT EXISTS `charlog` (
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
   `char_msg` varchar(255) NOT NULL default 'char select',
@@ -131,52 +122,29 @@ CREATE TABLE IF NOT EXISTS `charlog` (
   `luk` int(11) unsigned NOT NULL default '0',
   `hair` tinyint(4) NOT NULL default '0',
   `hair_color` int(11) NOT NULL default '0'
-) ENGINE=MyISAM; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `friends`
---
-
+DROP TABLE IF EXISTS `friends`;
 CREATE TABLE IF NOT EXISTS `friends` (
   `char_id` int(11) NOT NULL default '0',
   `friend_account` int(11) NOT NULL default '0',
   `friend_id` int(11) NOT NULL default '0',
-  KEY  `char_id` (`char_id`)
-) ENGINE=MyISAM;
+  KEY `char_id` (`char_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `hotkey`
---
-
-CREATE TABLE IF NOT EXISTS `hotkey` (
-	`char_id` INT(11) NOT NULL,
-	`hotkey` TINYINT(2) unsigned NOT NULL,
-	`type` TINYINT(1) unsigned NOT NULL default '0',
-	`itemskill_id` INT(11) unsigned NOT NULL default '0',
-	`skill_lvl` TINYINT(4) unsigned NOT NULL default '0',
-	PRIMARY KEY (`char_id`,`hotkey`),
-	INDEX (`char_id`)
-) ENGINE=MyISAM;
-
---
--- Table structure for table `global_reg_value`
---
-
+DROP TABLE IF EXISTS `global_reg_value`;
 CREATE TABLE IF NOT EXISTS `global_reg_value` (
   `char_id` int(11) unsigned NOT NULL default '0',
   `str` varchar(255) NOT NULL default '',
   `value` varchar(255) NOT NULL default '0',
-  `type` tinyint(1) NOT NULL default '3',
+  `type` tinyint(1) unsigned NOT NULL default '3',
   `account_id` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`char_id`,`str`,`account_id`),
   KEY `account_id` (`account_id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild`
---
-
+DROP TABLE IF EXISTS `guild`;
 CREATE TABLE IF NOT EXISTS `guild` (
   `guild_id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(24) NOT NULL default '',
@@ -197,12 +165,9 @@ CREATE TABLE IF NOT EXISTS `guild` (
   PRIMARY KEY  (`guild_id`,`char_id`),
   UNIQUE KEY `guild_id` (`guild_id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `guild_alliance`
---
-
+DROP TABLE IF EXISTS `guild_alliance`;
 CREATE TABLE IF NOT EXISTS `guild_alliance` (
   `guild_id` int(11) unsigned NOT NULL default '0',
   `opposition` int(11) unsigned NOT NULL default '0',
@@ -210,12 +175,9 @@ CREATE TABLE IF NOT EXISTS `guild_alliance` (
   `name` varchar(24) NOT NULL default '',
   PRIMARY KEY  (`guild_id`,`alliance_id`),
   KEY `alliance_id` (`alliance_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_castle`
---
-
+DROP TABLE IF EXISTS `guild_castle`;
 CREATE TABLE IF NOT EXISTS `guild_castle` (
   `castle_id` int(11) unsigned NOT NULL default '0',
   `guild_id` int(11) unsigned NOT NULL default '0',
@@ -237,24 +199,18 @@ CREATE TABLE IF NOT EXISTS `guild_castle` (
   `visibleG7` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`castle_id`),
   KEY `guild_id` (`guild_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_expulsion`
---
-
+DROP TABLE IF EXISTS `guild_expulsion`;
 CREATE TABLE IF NOT EXISTS `guild_expulsion` (
   `guild_id` int(11) unsigned NOT NULL default '0',
   `account_id` int(11) unsigned NOT NULL default '0',
   `name` varchar(24) NOT NULL default '',
   `mes` varchar(40) NOT NULL default '',
   PRIMARY KEY  (`guild_id`,`name`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_member`
---
-
+DROP TABLE IF EXISTS `guild_member`;
 CREATE TABLE IF NOT EXISTS `guild_member` (
   `guild_id` int(11) unsigned NOT NULL default '0',
   `account_id` int(11) unsigned NOT NULL default '0',
@@ -271,12 +227,9 @@ CREATE TABLE IF NOT EXISTS `guild_member` (
   `name` varchar(24) NOT NULL default '',
   PRIMARY KEY  (`guild_id`,`char_id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_position`
---
-
+DROP TABLE IF EXISTS `guild_position`;
 CREATE TABLE IF NOT EXISTS `guild_position` (
   `guild_id` int(9) unsigned NOT NULL default '0',
   `position` tinyint(6) unsigned NOT NULL default '0',
@@ -285,23 +238,18 @@ CREATE TABLE IF NOT EXISTS `guild_position` (
   `exp_mode` tinyint(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`guild_id`,`position`),
   KEY `guild_id` (`guild_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_skill`
---
-
+DROP TABLE IF EXISTS `guild_skill`;
 CREATE TABLE IF NOT EXISTS `guild_skill` (
   `guild_id` int(11) unsigned NOT NULL default '0',
   `id` smallint(11) unsigned NOT NULL default '0',
   `lv` tinyint(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`guild_id`,`id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `guild_storage`
---
 
+DROP TABLE IF EXISTS `guild_storage`;
 CREATE TABLE IF NOT EXISTS `guild_storage` (
   `id` int(10) unsigned NOT NULL auto_increment,
   `guild_id` int(11) unsigned NOT NULL default '0',
@@ -316,14 +264,12 @@ CREATE TABLE IF NOT EXISTS `guild_storage` (
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
   `expire_time` int(11) unsigned NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `guild_id` (`guild_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `homunculus`
---
-
+DROP TABLE IF EXISTS `homunculus`;
 CREATE TABLE IF NOT EXISTS `homunculus` (
   `homun_id` int(11) NOT NULL auto_increment,
   `char_id` int(11) NOT NULL,
@@ -348,21 +294,28 @@ CREATE TABLE IF NOT EXISTS `homunculus` (
   `rename_flag` tinyint(2) NOT NULL default '0',
   `vaporize` tinyint(2) NOT NULL default '0',
   PRIMARY KEY  (`homun_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
--- 
--- Table structure for table `interlog`
---
 
+DROP TABLE IF EXISTS `hotkey`;
+CREATE TABLE IF NOT EXISTS `hotkey` (
+  `char_id` int(11) NOT NULL,
+  `hotkey` tinyint(2) unsigned NOT NULL,
+  `type` tinyint(1) unsigned NOT NULL default '0',
+  `itemskill_id` int(11) unsigned NOT NULL default '0',
+  `skill_lvl` tinyint(4) unsigned NOT NULL default '0',
+  PRIMARY KEY  (`char_id`,`hotkey`),
+  KEY `char_id` (`char_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `interlog`;
 CREATE TABLE IF NOT EXISTS `interlog` (
   `time` datetime NOT NULL default '0000-00-00 00:00:00',
   `log` varchar(255) NOT NULL default ''
-) ENGINE=MyISAM; 
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `inventory`
---
-
+DROP TABLE IF EXISTS `invetory`;
 CREATE TABLE IF NOT EXISTS `inventory` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `char_id` int(11) unsigned NOT NULL default '0',
@@ -377,28 +330,26 @@ CREATE TABLE IF NOT EXISTS `inventory` (
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
   `expire_time` int(11) unsigned NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `ipbanlist`
---
 
+DROP TABLE IF EXISTS `ipbanlist`;
 CREATE TABLE IF NOT EXISTS `ipbanlist` (
   `list` varchar(255) NOT NULL default '',
   `btime` datetime NOT NULL default '0000-00-00 00:00:00',
   `rtime` datetime NOT NULL default '0000-00-00 00:00:00',
   `reason` varchar(255) NOT NULL default '',
-  KEY (`list`)
-) ENGINE=MyISAM;
+  KEY `list` (`list`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `login`
---
 
+DROP TABLE IF EXISTS `login`;
 CREATE TABLE IF NOT EXISTS `login` (
   `account_id` int(11) unsigned NOT NULL auto_increment,
+  `member_id` int(11) unsigned NOT NULL default '0',
   `userid` varchar(23) NOT NULL default '',
   `user_pass` varchar(32) NOT NULL default '',
   `sex` enum('M','F','S') NOT NULL default 'M',
@@ -410,49 +361,16 @@ CREATE TABLE IF NOT EXISTS `login` (
   `logincount` mediumint(9) unsigned NOT NULL default '0',
   `lastlogin` datetime NOT NULL default '0000-00-00 00:00:00',
   `last_ip` varchar(100) NOT NULL default '',
-  `birthdate` DATE NOT NULL DEFAULT '0000-00-00',
+  `birthdate` date NOT NULL default '0000-00-00',
+  `error_message` smallint(5) unsigned NOT NULL default '0',
+  `memo` smallint(5) unsigned NOT NULL default '0',
+  `ipallow` varchar(20) NOT NULL default '*.*.*.*',
   PRIMARY KEY  (`account_id`),
-  KEY `name` (`userid`)
-) ENGINE=MyISAM AUTO_INCREMENT=2000000; 
+  KEY `name` (`userid`),
+  KEY `member_id` (`member_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2000000 ;
 
--- added standard accounts for servers, VERY INSECURE!!!
--- inserted into the table called login which is above
-
-INSERT INTO `login` (`account_id`, `userid`, `user_pass`, `sex`, `email`) VALUES ('1', 's1', 'p1', 'S','athena@athena.com');
-
---
--- Table structure for table `mapreg`
---
-
-CREATE TABLE IF NOT EXISTS `mapreg` (
-  `varname` varchar(32) NOT NULL,
-  `index` int(11) unsigned NOT NULL default '0',
-  `value` varchar(255) NOT NULL,
-  KEY `varname` (`varname`),
-  KEY `index` (`index`)
-) ENGINE=MyISAM;
-
---
--- Table structure for table `sc_data`
---
-
-CREATE TABLE IF NOT EXISTS `sc_data` (
-  `account_id` int(11) unsigned NOT NULL,
-  `char_id` int(11) unsigned NOT NULL,
-  `type` smallint(11) unsigned NOT NULL,
-  `tick` int(11) NOT NULL,
-  `val1` int(11) NOT NULL default '0',
-  `val2` int(11) NOT NULL default '0',
-  `val3` int(11) NOT NULL default '0',
-  `val4` int(11) NOT NULL default '0',
-  KEY (`account_id`),
-  KEY (`char_id`)
-) ENGINE=MyISAM;
-
---
--- Table structure for table `mail`
---
-
+DROP TABLE IF EXISTS `mail`;
 CREATE TABLE IF NOT EXISTS `mail` (
   `id` bigint(20) unsigned NOT NULL auto_increment,
   `send_name` varchar(30) NOT NULL default '',
@@ -473,13 +391,22 @@ CREATE TABLE IF NOT EXISTS `mail` (
   `card1` smallint(11) NOT NULL default '0',
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `memo`
---
 
+DROP TABLE IF EXISTS `mapreg`;
+CREATE TABLE IF NOT EXISTS `mapreg` (
+  `varname` varchar(32) NOT NULL,
+  `index` int(11) unsigned NOT NULL default '0',
+  `value` varchar(255) NOT NULL,
+  KEY `varname` (`varname`),
+  KEY `index` (`index`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `memo`;
 CREATE TABLE IF NOT EXISTS `memo` (
   `memo_id` int(11) unsigned NOT NULL auto_increment,
   `char_id` int(11) unsigned NOT NULL default '0',
@@ -488,12 +415,10 @@ CREATE TABLE IF NOT EXISTS `memo` (
   `y` smallint(4) unsigned NOT NULL default '0',
   PRIMARY KEY  (`memo_id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `mercenary`
---
 
+DROP TABLE IF EXISTS `mercenary`;
 CREATE TABLE IF NOT EXISTS `mercenary` (
   `mer_id` int(11) unsigned NOT NULL auto_increment,
   `char_id` int(11) NOT NULL,
@@ -503,12 +428,10 @@ CREATE TABLE IF NOT EXISTS `mercenary` (
   `kill_counter` int(11) NOT NULL,
   `life_time` int(11) NOT NULL default '0',
   PRIMARY KEY  (`mer_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `mercenary_owner`
---
 
+DROP TABLE IF EXISTS `mercenary_owner`;
 CREATE TABLE IF NOT EXISTS `mercenary_owner` (
   `char_id` int(11) NOT NULL,
   `merc_id` int(11) NOT NULL default '0',
@@ -519,12 +442,10 @@ CREATE TABLE IF NOT EXISTS `mercenary_owner` (
   `sword_calls` int(11) NOT NULL default '0',
   `sword_faith` int(11) NOT NULL default '0',
   PRIMARY KEY  (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `party`
---
 
+DROP TABLE IF EXISTS `party`;
 CREATE TABLE IF NOT EXISTS `party` (
   `party_id` int(11) unsigned NOT NULL auto_increment,
   `name` varchar(24) NOT NULL default '',
@@ -533,12 +454,10 @@ CREATE TABLE IF NOT EXISTS `party` (
   `leader_id` int(11) unsigned NOT NULL default '0',
   `leader_char` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`party_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `pet`
---
 
+DROP TABLE IF EXISTS `pet`;
 CREATE TABLE IF NOT EXISTS `pet` (
   `pet_id` int(11) unsigned NOT NULL auto_increment,
   `class` mediumint(9) unsigned NOT NULL default '0',
@@ -553,12 +472,9 @@ CREATE TABLE IF NOT EXISTS `pet` (
   `rename_flag` tinyint(4) unsigned NOT NULL default '0',
   `incuvate` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`pet_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Table structure for table `quest`
---
-
+DROP TABLE IF EXISTS `quest`;
 CREATE TABLE IF NOT EXISTS `quest` (
   `char_id` int(11) unsigned NOT NULL default '0',
   `quest_id` int(10) unsigned NOT NULL,
@@ -568,58 +484,58 @@ CREATE TABLE IF NOT EXISTS `quest` (
   `count2` mediumint(8) unsigned NOT NULL default '0',
   `count3` mediumint(8) unsigned NOT NULL default '0',
   PRIMARY KEY  (`char_id`,`quest_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `ragsrvinfo`
---
-
+DROP TABLE IF EXISTS `ragsrvinfo`;
 CREATE TABLE IF NOT EXISTS `ragsrvinfo` (
   `index` int(11) NOT NULL default '0',
   `name` varchar(255) NOT NULL default '',
   `exp` int(11) unsigned NOT NULL default '0',
   `jexp` int(11) unsigned NOT NULL default '0',
   `drop` int(11) unsigned NOT NULL default '0'
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `skill`
---
 
+DROP TABLE IF EXISTS `sc_data`;
+CREATE TABLE IF NOT EXISTS `sc_data` (
+  `account_id` int(11) unsigned NOT NULL,
+  `char_id` int(11) unsigned NOT NULL,
+  `type` smallint(11) unsigned NOT NULL,
+  `tick` int(11) NOT NULL,
+  `val1` int(11) NOT NULL default '0',
+  `val2` int(11) NOT NULL default '0',
+  `val3` int(11) NOT NULL default '0',
+  `val4` int(11) NOT NULL default '0',
+  KEY `account_id` (`account_id`),
+  KEY `char_id` (`char_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `skill`;
 CREATE TABLE IF NOT EXISTS `skill` (
   `char_id` int(11) unsigned NOT NULL default '0',
   `id` smallint(11) unsigned NOT NULL default '0',
   `lv` tinyint(4) unsigned NOT NULL default '0',
   PRIMARY KEY  (`char_id`,`id`),
   KEY `char_id` (`char_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `skill_homunculus`
---
-
+DROP TABLE IF EXISTS `skill_homunculus`;
 CREATE TABLE IF NOT EXISTS `skill_homunculus` (
   `homun_id` int(11) NOT NULL,
   `id` int(11) NOT NULL,
   `lv` smallint(6) NOT NULL,
   PRIMARY KEY  (`homun_id`,`id`),
   KEY `homun_id` (`homun_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `sstatus`
---
-
+DROP TABLE IF EXISTS `sstatus`;
 CREATE TABLE IF NOT EXISTS `sstatus` (
   `index` tinyint(4) unsigned NOT NULL default '0',
   `name` varchar(255) NOT NULL default '',
   `user` int(11) unsigned NOT NULL default '0'
-) ENGINE=MyISAM;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `storage`
---
-
+DROP TABLE IF EXISTS `storage`;
 CREATE TABLE IF NOT EXISTS `storage` (
   `id` int(11) unsigned NOT NULL auto_increment,
   `account_id` int(11) unsigned NOT NULL default '0',
@@ -634,6 +550,10 @@ CREATE TABLE IF NOT EXISTS `storage` (
   `card2` smallint(11) NOT NULL default '0',
   `card3` smallint(11) NOT NULL default '0',
   `expire_time` int(11) unsigned NOT NULL default '0',
+  `serial` int(11) unsigned NOT NULL default '0',
   PRIMARY KEY  (`id`),
   KEY `account_id` (`account_id`)
-) ENGINE=MyISAM;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+
+INSERT INTO `login` (`account_id`, `userid`, `user_pass`, `sex`, `email`) VALUES ('1', 's1', 'p1', 'S','athena@athena.com');
